cee4527c73f467e5dacc30c197bd3c66a7afc61e
NathanMartinez <natedzmtz@yahoo.com>
2021-11-29 01:41:55 -0700

    Adding missing changes.

--- node_server/Models/user.js
+++ node_server/Models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
   },
   password: String,
-  phone_number: Number,
+  phone_number: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
